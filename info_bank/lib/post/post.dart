069b0871dcc6836856431655d05ea3ee7088a596
DrWoose <usa30311@gmail.com>
2023-04-02 14:02:27 +0800

    mainpage filter&post update

--- info_bank/lib/post/post.dart
+++ info_bank/lib/post/post.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Post {
+  final String title;
+  final String tag;
+  final double Best;
+  final double unlocked;
+  const Post({
+    required this.title,
+    required this.tag,
+    required this.Best,
+    required this.unlocked,
+  });
+}
+
+const allPost = [
+  Post(title: 'Post 1', tag: 'follow', Best: 89.1, unlocked: 3000),
+  Post(title: 'Post 2', tag: 'follow', Best: 71.1, unlocked: 1234),
+  Post(title: 'Post 3', tag: 'follow', Best: 67.4, unlocked: 5678),
+  Post(title: 'Post 4', tag: 'recommend', Best: 82.2, unlocked: 9876),
+  Post(title: 'Post 5', tag: 'recommend', Best: 23.3, unlocked: 2351),
+];
 
 class MyPost extends StatelessWidget {
-  final String child;
+  final int index;
 
-  MyPost({required this.child});
+  MyPost({required this.index});
 
   @override
   Widget build(BuildContext context) {
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Container(
+          height: 100,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: 10,
+                  left: 10,
+                  child: Text(
+                    //title
+                    allPost[index].title,
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                    ),
+                  )),
+              Positioned(
+                  //tags
+                  bottom: 10,
+                  left: 10,
+                  child: Text(
+                    allPost[index].tag,
+                    style: GoogleFonts.openSans(),
+                  )),
+              Positioned(
+                  //bestanswer
+                  bottom: 30,
+                  right: 10,
+                  child: Text(
+                    "最佳回答：" + allPost[index].Best.toString() + "%",
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              Positioned(
+                  //answerpotion
+                  bottom: 10,
+                  right: 10,
+                  child: Text(
+                    "解鎖次數：" +
+                        (allPost[index].unlocked / 1000).toStringAsFixed(1) +
+                        "k",
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))
+            ],
+          ),
+        ));
+  }
+}
+
+
+
+// Origin
+/* 
+
+Container(
         height: 400,
         color: Colors.deepPurple[200],
         child: Center(
@@ -17,7 +97,136 @@ class MyPost extends StatelessWidget {
           child,
           style: TextStyle(fontSize: 40),
         )),
+),
+*/
+
+//modified
+/*
+Container(
+        padding: EdgeInsets.fromLTRB(4 * fem, 3 * fem, 6 * fem, 4 * fem),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xffd9d9d9),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                '古城麻辣燙排隊狀況',
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(225 * fem, 0 * fem, 0 * fem, 0 * fem),
+              width: 82 * fem,
+              height: 16 * fem,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0 * fem,
+                    top: 0 * fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 82 * fem,
+                        height: 16 * fem,
+                        child: Text(
+                          '最佳回答：72%',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0 * fem,
+                    top: 0 * fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 82 * fem,
+                        height: 16 * fem,
+                        child: Text(
+                          '最佳回答：72%',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(11 * fem, 0 * fem, 0 * fem, 0 * fem),
+              height: 20 * fem,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 10 * fem, 0 * fem),
+                    width: 41 * fem,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(12 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '古城',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 159 * fem, 0 * fem),
+                    width: 41 * fem,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(12 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '排隊',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 83 * fem,
+                    height: 16 * fem,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0 * fem,
+                          top: 0 * fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 83 * fem,
+                              height: 16 * fem,
+                              child: Text(
+                                '解鎖次數：2.7k',
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 0 * fem,
+                          top: 0 * fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 83 * fem,
+                              height: 16 * fem,
+                              child: Text(
+                                '解鎖次數：2.7k',
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
+*/
--- info_bank/lib/screens/home.dart
+++ info_bank/lib/screens/home.dart
@@ -13,29 +13,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  //var post = <SizedBox>[];
-  final List _post = [
-    'post1',
-    'post2',
-    'post3',
-    'post4',
-    'post5',
-  ];
-
-  int cnt = 0;
-  void addpost() {
-    _post.add(
-      SizedBox(height: 16.0),
-    );
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  double FilterWidth = 88;
+  List<Post> _currentposts = allPost;
+  callback(changedPost, varFilterWidth) {
+    setState(() {
+      _currentposts = changedPost;
+      FilterWidth = varFilterWidth;
+    });
   }
 
+  int cnt = 0;
   @override
   Widget build(BuildContext context) {
     double baseWidth = MediaQuery.of(context).size.width;
     double baseHeight = MediaQuery.of(context).size.height;
-    String dropdownValue = '全部';
-    double FilterWidth = 76; // width 76 100 136
+    String dropdownValue = '未篩選';
+
     return Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           titleSpacing: 0.0,
           title: Row(
@@ -47,19 +43,9 @@ class _HomeState extends State<Home> {
                   Icons.list,
                   color: Colors.black,
                 ),
-                onPressed: () => Scaffold.of(context).openDrawer(),
+                onPressed: () => _scaffoldKey.currentState?.openDrawer(),
               ),
-              Stack(
-                children: <Widget>[
-                  Expanded(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                          maxWidth: FilterWidth), // max text length
-                      child: HomeFilter(),
-                    ),
-                  ),
-                ],
-              ),
+              HomeFilter(callback: callback),
               Expanded(
                 child: Center(
                   child: Text(
@@ -69,27 +55,13 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ),
-              /*
-              IconButton(
-                  color: Colors.black,
-                  icon: const Icon(Icons.arrow_drop_down_sharp),
-                  onPressed: () {
-                    //Navigator.pushNamed(context, '/search');
-                  }),*/
+              SizedBox(
+                width: FilterWidth,
+              )
             ],
           ),
           automaticallyImplyLeading: false,
           centerTitle: true,
-          /*
-          leading: Builder(
-            builder: (BuildContext context) {
-              return Row(
-                children: [
-                  
-                ],
-              );
-            },
-          ),*/
           actions: <Widget>[
             Row(
               children: <Widget>[
@@ -111,11 +83,9 @@ class _HomeState extends State<Home> {
             Expanded(
               child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: _post.length,
+                  itemCount: _currentposts.length,
                   itemBuilder: (context, index) {
-                    return MyPost(
-                      child: _post[index],
-                    );
+                    return MyPost(index: index);
                   }),
             ),
           ],
@@ -123,83 +93,112 @@ class _HomeState extends State<Home> {
   }
 }
 
-List<String> list = <String>['全部', '追蹤頻道', '個人屬性推薦'];
+List<String> list = <String>['未篩選', '追蹤頻道'];
 
 class HomeFilter extends StatefulWidget {
-  const HomeFilter({super.key});
+  final Function callback;
+  const HomeFilter({super.key, required this.callback});
 
   @override
   State<HomeFilter> createState() => _HomeFilter();
 }
 
 class _HomeFilter extends State<HomeFilter> {
+  double FilterWidth = 88; // width 88 100
+
   String selectedValue = list.first;
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton2(
-        isExpanded: true,
-        items: list
-            .map((item) => DropdownMenuItem<String>(
-                  value: item,
-                  child: Text(
-                    item,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+    return SizedBox(
+      width: FilterWidth,
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2(
+          isExpanded: true,
+          items: list
+              .map((item) => DropdownMenuItem<String>(
+                    value: item,
+                    child: Text(
+                      item,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ))
-            .toList(),
-        value: selectedValue,
-        onChanged: (value) {
-          setState(() {
+                  ))
+              .toList(),
+          value: selectedValue,
+          onChanged: (value) {
+            if (selectedValue != value) {
+              switch (value) {
+                case "未篩選":
+                  FilterWidth = 88;
+                  widget.callback(filterResults(0), FilterWidth);
+                  break;
+                case "追蹤頻道":
+                  FilterWidth = 100;
+                  widget.callback(filterResults(1), FilterWidth);
+                  break;
+              }
+            }
             selectedValue = value as String;
-          });
-        },
-        buttonStyleData: ButtonStyleData(
-          height: 50,
-          width: 180,
-          padding: const EdgeInsets.only(left: 14, right: 14),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(14),
-            border: Border.all(
-              color: Colors.black26,
-            ),
-            color: Color(0xff272727),
-          ),
-          elevation: 2,
-        ),
-        iconStyleData: const IconStyleData(
-          icon: Icon(
-            Icons.arrow_forward_ios_outlined,
-          ),
-          iconSize: 14,
-          iconEnabledColor: Colors.yellow,
-          iconDisabledColor: Colors.grey,
-        ),
-        dropdownStyleData: DropdownStyleData(
-            maxHeight: 200,
-            width: 200,
-            padding: null,
+          },
+          buttonStyleData: ButtonStyleData(
+            height: 50,
+            width: 180,
+            padding: const EdgeInsets.only(left: 14, right: 14),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(14),
+              border: Border.all(
+                color: Colors.black26,
+              ),
               color: Color(0xff272727),
             ),
-            elevation: 8,
-            offset: const Offset(-20, 0),
-            scrollbarTheme: ScrollbarThemeData(
-              radius: const Radius.circular(40),
-              thickness: MaterialStateProperty.all(6),
-              thumbVisibility: MaterialStateProperty.all(true),
-            )),
-        menuItemStyleData: const MenuItemStyleData(
-          height: 40,
-          padding: EdgeInsets.only(left: 14, right: 14),
+            elevation: 2,
+          ),
+          iconStyleData: const IconStyleData(
+            icon: Icon(
+              Icons.arrow_forward_ios_outlined,
+            ),
+            iconSize: 14,
+            iconEnabledColor: Colors.yellow,
+            iconDisabledColor: Colors.grey,
+          ),
+          dropdownStyleData: DropdownStyleData(
+              maxHeight: 200,
+              width: 200,
+              padding: null,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                color: Color(0xff272727),
+              ),
+              elevation: 8,
+              offset: const Offset(-20, 0),
+              scrollbarTheme: ScrollbarThemeData(
+                radius: const Radius.circular(40),
+                thickness: MaterialStateProperty.all(6),
+                thumbVisibility: MaterialStateProperty.all(true),
+              )),
+          menuItemStyleData: const MenuItemStyleData(
+            height: 40,
+            padding: EdgeInsets.only(left: 14, right: 14),
+          ),
         ),
       ),
     );
+  }
+
+  List<Post> filterResults(int val) {
+    int i = 0;
+    String filtertag = '';
+    if (val == 1) {
+      filtertag = 'follow';
+    }
+    print("filterResults");
+    return allPost.where((_currentpost) {
+      final Posttag = _currentpost.tag;
+      return Posttag.contains(filtertag);
+    }).toList();
   }
 }
