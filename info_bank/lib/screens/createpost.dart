92522c5917c2f77bb7025a9f1e98d4abe73f73a3
rTheDev <98320894+rTheDev@users.noreply.github.com>
2023-04-09 19:09:25 +0800

    remove const

--- info_bank/lib/screens/createpost.dart
+++ info_bank/lib/screens/createpost.dart
@@ -105,7 +105,7 @@ class _CreatePostState extends State<CreatePost> {
                         borderRadius: BorderRadius.circular(10),
                       ),
                       //height: 120,
-                      child: const Stack(
+                      child: Stack(
                         children: [
                           SizedBox(
                               height: 120,
@@ -282,7 +282,7 @@ class _CreatePostState extends State<CreatePost> {
                         borderRadius: BorderRadius.circular(10),
                       ),
                       //height: 120,
-                      child: const Stack(
+                      child: Stack(
                         children: [
                           SizedBox(
                               height: 120,
