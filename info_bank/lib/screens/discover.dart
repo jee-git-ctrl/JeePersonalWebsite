62b65da2bc955dad76e687521426afcfcde076a0
usa30311 <usa30311@gmail.com>
2023-04-23 19:52:37 +0800

    discover half done

--- info_bank/lib/screens/discover.dart
+++ info_bank/lib/screens/discover.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:info_bank/sidemenu/side_menu.dart';
 import 'package:info_bank/screens/search.dart';
-
-import 'package:info_bank/screens/reportpage.dart';
+import 'package:info_bank/post/post.dart';
 
 class Discover extends StatefulWidget {
   @override
@@ -10,89 +9,65 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
+  List<Post> _currentposts = allPost;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SideMenu(),
-      appBar: AppBar(
-        leading: Builder(
-          builder: (BuildContext context) {
-            return SizedBox(
-              child: IconButton(
-                icon: const Icon(
-                  Icons.list,
-                  color: Colors.black,
+        drawer: SideMenu(),
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return SizedBox(
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.list,
+                    color: Colors.black,
+                  ),
+                  onPressed: () => Scaffold.of(context).openDrawer(),
                 ),
-                onPressed: () => Scaffold.of(context).openDrawer(),
-              ),
-            );
-          },
-        ),
-        //backgroundColor: Color(0xffd9d9d9),
-        elevation: 0.0,
-        actions: <Widget>[
-          Row(
-            children: <Widget>[
-              IconButton(
-                  color: Colors.black,
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => Search()));
-                    //Navigator.pushNamed(context, '/search');
-                  }),
-            ],
-          ),
-        ],
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              if (context.mounted) {
-                showReportDialog(context);
-              }
+              );
             },
-            child: const Text('Debug Report')),
-      ),
-    );
-  }
-}
-
-List<String> list = <String>['全部', '追蹤頻道', '個人屬性推薦'];
-
-class HomeFilter extends StatefulWidget {
-  const HomeFilter({super.key});
-
-  @override
-  State<HomeFilter> createState() => _HomeFilter();
-}
-
-class _HomeFilter extends State<HomeFilter> {
-  String dropdownValue = list.first;
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
+          ),
+          //backgroundColor: Color(0xffd9d9d9),
+          elevation: 0.0,
+          title: Text("探索"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            ConstrainedBox(
+              constraints: new BoxConstraints(
+                minHeight: 35.0,
+                maxHeight: 150.0,
+              ),
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(8.0),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 150,
+                        width: 100,
+                        decoration: BoxDecoration(color: Colors.black),
+                        child: Text("hi"),
+                      ),
+                    );
+                  }),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _currentposts.length,
+                  itemBuilder: (context, index) {
+                    return MyPost(currentpost: _currentposts[index]);
+                  }),
+            ),
+          ],
+        ));
   }
 }
