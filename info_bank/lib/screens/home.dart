90d4910e78dcfd29f72f0d91ca9e8fb458bd1ead
usa30311 <usa30311@gmail.com>
2023-04-28 23:04:51 +0800

    home dropdown botton change

--- info_bank/lib/screens/home.dart
+++ info_bank/lib/screens/home.dart
@@ -147,7 +147,7 @@ class _HomeFilter extends State<HomeFilter> {
           },
           buttonStyleData: ButtonStyleData(
             height: 50,
-            width: 180,
+            width: 120,
             padding: const EdgeInsets.only(left: 14, right: 14),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(14),
@@ -168,14 +168,14 @@ class _HomeFilter extends State<HomeFilter> {
           ),
           dropdownStyleData: DropdownStyleData(
               maxHeight: 200,
-              width: 200,
+              width: 120,
               padding: null,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(14),
                 color: Color(0xff272727),
               ),
               elevation: 8,
-              offset: const Offset(-20, 0),
+              offset: const Offset(0, 0),
               scrollbarTheme: ScrollbarThemeData(
                 radius: const Radius.circular(40),
                 thickness: MaterialStateProperty.all(6),
